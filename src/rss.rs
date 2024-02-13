50f3b7fc80c05b6712b2972ab2e734fda77c5c67
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2024-02-13 18:16:06 +0900

    fix: user-agentをつけてリクエストするようにした

--- src/rss.rs
+++ src/rss.rs
@@ -1,16 +1,25 @@
-use std::error::Error;
 use feed_rs::{model::Feed, parser};
+use std::error::Error;
 
 #[derive(Clone)]
-pub struct RssProvider;
+pub struct RssProvider {
+    client: reqwest::Client,
+}
 
 impl RssProvider {
     pub fn new() -> RssProvider {
-        RssProvider {}
+        let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36";
+        let client = reqwest::ClientBuilder::new()
+            .user_agent(user_agent.to_string())
+            .build()
+            .unwrap();
+
+        RssProvider { client }
     }
 
     pub async fn get_rss_feeds(&self, url: String) -> Result<Feed, Box<dyn Error>> {
-        let content = reqwest::get(url).await?.bytes().await?;
+        let client = self.client.clone();
+        let content = client.get(url.clone()).send().await?.bytes().await?;
 
         let feed = parser::parse(&content[..])?;
 
